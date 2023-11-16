e0c52e1e803e56eb036e5bf3c258c82d55225ce0
nuwan saranath <saranathnuwan@gmail.com>
2023-11-17 00:04:34 +0530

    Create a database connection

--- pos-system/src/main/java/lk/ijse/dep11/controller/ManageCustomerController.java
+++ pos-system/src/main/java/lk/ijse/dep11/controller/ManageCustomerController.java
@@ -1,11 +1,25 @@
 package lk.ijse.dep11.controller;
 
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.dep11.tm.Customer;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
 
 public class ManageCustomerController {
     public AnchorPane root;
@@ -16,22 +30,24 @@ public class ManageCustomerController {
     public TextField txtAddress;
     public Button btnDelete;
     public ImageView imgHome;
-    public TableView tblCustomer;
+    public TableView<Customer> tblCustomer;
+    public TextField txtPhoneNumber;
+
+    public void navigateToHome(MouseEvent mouseEvent) throws IOException {
+    }
 
     public void initialize(){
-        btnSave.setDisable(true);
-        btnDelete.setDisable(true);
-        btnNewCustomer.fire();
-
 
 
     }
-
     public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
+
     }
     public void btnDeleteOnAction(ActionEvent actionEvent) {
+
     }
 }
---
+++ pos-system/src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS customer
+(
+    id      VARCHAR(10) PRIMARY KEY,
+    name    VARCHAR(150) NOT NULL,
+    address VARCHAR(400) NOT NULL,
+    contact_number VARCHAR(15) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS item
+(
+    code        VARCHAR(30) PRIMARY KEY,
+    description VARCHAR(200) NOT NULL,
+    qty         INT,
+    unit_price  DECIMAL(8, 2)
+    );
+
+CREATE TABLE IF NOT EXISTS `order`
+(
+    id          VARCHAR(10) PRIMARY KEY,
+    date        DATE NOT NULL,
+    customer_id VARCHAR(10) NOT NULL,
+    CONSTRAINT fk_order FOREIGN KEY (customer_id) REFERENCES customer (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS order_item
+(
+    order_id   VARCHAR(10),
+    item_code  VARCHAR(30),
+    CONSTRAINT pk_order_item PRIMARY KEY (order_id, item_code),
+    qty        INT           NOT NULL,
+    unit_price DECIMAL(8, 2) NOT NULL,
+    CONSTRAINT fk_order_item1 FOREIGN KEY (order_id) REFERENCES `order` (id),
+    CONSTRAINT fk_order_item2 FOREIGN KEY (item_code) REFERENCES item (code)
+    );
